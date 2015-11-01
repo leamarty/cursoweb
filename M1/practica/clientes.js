e8763ca9746a3b50a0baa1c103a789d5b520ceba
Leandro <leanmarty@gmail.com>
2015-11-01 18:54:09 -0300

    Agrego DB clietnes

---
+++ M1/practica/clientes.js
@@ -0,0 +1,632 @@
+var clientes = [
+  {
+    "id": 1,
+    "razonSocial": "OLYMPIX",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=1&w=32&h=32",
+    "direccion": {
+      "calle": "Clarkson Avenue",
+      "altura": 257,
+      "ciudad": "Orviston",
+      "pais": "Vatican City State (Holy See)"
+    },
+    "activo": "2286.16",
+    "pasivo": "2901.09",
+    "presidente": {
+      "nombre": "Castillo",
+      "apellido": "Hubbard"
+    },
+    "fechaCreacion": "Monday, May 19, 2014 12:39 PM"
+  },
+  {
+    "id": 2,
+    "razonSocial": "ZOLAREX",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=2&w=32&h=32",
+    "direccion": {
+      "calle": "Noble Street",
+      "altura": 690,
+      "ciudad": "Hailesboro",
+      "pais": "Christmas Island"
+    },
+    "activo": "1022.73",
+    "pasivo": "2511.16",
+    "presidente": {
+      "nombre": "Allyson",
+      "apellido": "Morse"
+    },
+    "fechaCreacion": "Wednesday, August 5, 2015 8:56 AM"
+  },
+  {
+    "id": 3,
+    "razonSocial": "ZENTILITY",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=3&w=32&h=32",
+    "direccion": {
+      "calle": "Powers Street",
+      "altura": 888,
+      "ciudad": "Winesburg",
+      "pais": "Uruguay"
+    },
+    "activo": "1419.32",
+    "pasivo": "1185.12",
+    "presidente": {
+      "nombre": "Riggs",
+      "apellido": "Klein"
+    },
+    "fechaCreacion": "Tuesday, July 15, 2014 12:52 AM"
+  },
+  {
+    "id": 4,
+    "razonSocial": "ENERSOL",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=4&w=32&h=32",
+    "direccion": {
+      "calle": "Coffey Street",
+      "altura": 211,
+      "ciudad": "Vicksburg",
+      "pais": "Turks and Caicos Islands"
+    },
+    "activo": "2020.44",
+    "pasivo": "3304.81",
+    "presidente": {
+      "nombre": "Juliet",
+      "apellido": "Alford"
+    },
+    "fechaCreacion": "Tuesday, March 18, 2014 5:50 PM"
+  },
+  {
+    "id": 5,
+    "razonSocial": "PUSHCART",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=5&w=32&h=32",
+    "direccion": {
+      "calle": "Huntington Street",
+      "altura": 280,
+      "ciudad": "Westwood",
+      "pais": "Morocco"
+    },
+    "activo": "2696.93",
+    "pasivo": "2668.63",
+    "presidente": {
+      "nombre": "Ida",
+      "apellido": "Farley"
+    },
+    "fechaCreacion": "Friday, December 19, 2014 12:25 AM"
+  },
+  {
+    "id": 6,
+    "razonSocial": "ASSISTIA",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=6&w=32&h=32",
+    "direccion": {
+      "calle": "Navy Walk",
+      "altura": 486,
+      "ciudad": "Blanford",
+      "pais": "Moldova"
+    },
+    "activo": "2471.71",
+    "pasivo": "3751.52",
+    "presidente": {
+      "nombre": "Rena",
+      "apellido": "Shepherd"
+    },
+    "fechaCreacion": "Monday, September 28, 2015 12:28 PM"
+  },
+  {
+    "id": 7,
+    "razonSocial": "TALKALOT",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=7&w=32&h=32",
+    "direccion": {
+      "calle": "Revere Place",
+      "altura": 255,
+      "ciudad": "Murillo",
+      "pais": "Ethiopia"
+    },
+    "activo": "3538.07",
+    "pasivo": "1805.84",
+    "presidente": {
+      "nombre": "Pat",
+      "apellido": "Alvarez"
+    },
+    "fechaCreacion": "Monday, September 28, 2015 7:33 AM"
+  },
+  {
+    "id": 8,
+    "razonSocial": "NIMON",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=8&w=32&h=32",
+    "direccion": {
+      "calle": "Rugby Road",
+      "altura": 353,
+      "ciudad": "Odessa",
+      "pais": "Eritrea"
+    },
+    "activo": "1639.79",
+    "pasivo": "1462.15",
+    "presidente": {
+      "nombre": "Bonner",
+      "apellido": "Haynes"
+    },
+    "fechaCreacion": "Wednesday, April 29, 2015 11:41 PM"
+  },
+  {
+    "id": 9,
+    "razonSocial": "ANARCO",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=9&w=32&h=32",
+    "direccion": {
+      "calle": "Chestnut Avenue",
+      "altura": 310,
+      "ciudad": "Kersey",
+      "pais": "Jamaica"
+    },
+    "activo": "1105.9",
+    "pasivo": "2908.87",
+    "presidente": {
+      "nombre": "Beverley",
+      "apellido": "Ingram"
+    },
+    "fechaCreacion": "Thursday, January 29, 2015 11:49 AM"
+  },
+  {
+    "id": 10,
+    "razonSocial": "SOLGAN",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=10&w=32&h=32",
+    "direccion": {
+      "calle": "Degraw Street",
+      "altura": 716,
+      "ciudad": "Craig",
+      "pais": "San Marino"
+    },
+    "activo": "3492.8",
+    "pasivo": "3381.92",
+    "presidente": {
+      "nombre": "Lang",
+      "apellido": "Glass"
+    },
+    "fechaCreacion": "Monday, August 18, 2014 1:37 PM"
+  },
+  {
+    "id": 11,
+    "razonSocial": "ZILCH",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=11&w=32&h=32",
+    "direccion": {
+      "calle": "Cooke Court",
+      "altura": 461,
+      "ciudad": "Southmont",
+      "pais": "Bermuda"
+    },
+    "activo": "2951.24",
+    "pasivo": "1599.84",
+    "presidente": {
+      "nombre": "Middleton",
+      "apellido": "Gill"
+    },
+    "fechaCreacion": "Monday, August 24, 2015 2:51 AM"
+  },
+  {
+    "id": 12,
+    "razonSocial": "ATGEN",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=12&w=32&h=32",
+    "direccion": {
+      "calle": "Dewey Place",
+      "altura": 894,
+      "ciudad": "Thomasville",
+      "pais": "Maldives"
+    },
+    "activo": "1962.2",
+    "pasivo": "2647.37",
+    "presidente": {
+      "nombre": "Terrie",
+      "apellido": "Kelly"
+    },
+    "fechaCreacion": "Monday, September 22, 2014 7:31 PM"
+  },
+  {
+    "id": 13,
+    "razonSocial": "PHUEL",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=13&w=32&h=32",
+    "direccion": {
+      "calle": "Montgomery Place",
+      "altura": 259,
+      "ciudad": "Kaka",
+      "pais": "Niger"
+    },
+    "activo": "1814.61",
+    "pasivo": "1573.6",
+    "presidente": {
+      "nombre": "Foley",
+      "apellido": "Emerson"
+    },
+    "fechaCreacion": "Tuesday, October 21, 2014 4:16 PM"
+  },
+  {
+    "id": 14,
+    "razonSocial": "PERKLE",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=14&w=32&h=32",
+    "direccion": {
+      "calle": "Jewel Street",
+      "altura": 659,
+      "ciudad": "Clara",
+      "pais": "Luxembourg"
+    },
+    "activo": "1064.5",
+    "pasivo": "3481.28",
+    "presidente": {
+      "nombre": "Short",
+      "apellido": "Bryant"
+    },
+    "fechaCreacion": "Thursday, January 30, 2014 4:22 PM"
+  },
+  {
+    "id": 15,
+    "razonSocial": "GLOBOIL",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=15&w=32&h=32",
+    "direccion": {
+      "calle": "Cass Place",
+      "altura": 139,
+      "ciudad": "Dragoon",
+      "pais": "Madagascar"
+    },
+    "activo": "3930.65",
+    "pasivo": "3186.65",
+    "presidente": {
+      "nombre": "Becker",
+      "apellido": "Cole"
+    },
+    "fechaCreacion": "Thursday, March 27, 2014 4:29 AM"
+  },
+  {
+    "id": 16,
+    "razonSocial": "ONTAGENE",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=16&w=32&h=32",
+    "direccion": {
+      "calle": "Claver Place",
+      "altura": 670,
+      "ciudad": "Celeryville",
+      "pais": "Sao Tome and Principe"
+    },
+    "activo": "1270.35",
+    "pasivo": "2894.74",
+    "presidente": {
+      "nombre": "Reynolds",
+      "apellido": "Macdonald"
+    },
+    "fechaCreacion": "Wednesday, February 19, 2014 3:28 AM"
+  },
+  {
+    "id": 17,
+    "razonSocial": "EMOLTRA",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=17&w=32&h=32",
+    "direccion": {
+      "calle": "Seacoast Terrace",
+      "altura": 489,
+      "ciudad": "Waiohinu",
+      "pais": "Mozambique"
+    },
+    "activo": "3534.94",
+    "pasivo": "3469.69",
+    "presidente": {
+      "nombre": "Walton",
+      "apellido": "Lyons"
+    },
+    "fechaCreacion": "Sunday, April 27, 2014 12:08 AM"
+  },
+  {
+    "id": 18,
+    "razonSocial": "QUIZMO",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=18&w=32&h=32",
+    "direccion": {
+      "calle": "Hudson Avenue",
+      "altura": 505,
+      "ciudad": "Lupton",
+      "pais": "Bahamas"
+    },
+    "activo": "2287.33",
+    "pasivo": "1370.35",
+    "presidente": {
+      "nombre": "Ballard",
+      "apellido": "Raymond"
+    },
+    "fechaCreacion": "Tuesday, March 31, 2015 3:45 PM"
+  },
+  {
+    "id": 19,
+    "razonSocial": "BOINK",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=19&w=32&h=32",
+    "direccion": {
+      "calle": "Frost Street",
+      "altura": 213,
+      "ciudad": "Albany",
+      "pais": "Yugoslavia"
+    },
+    "activo": "2982.49",
+    "pasivo": "3797.74",
+    "presidente": {
+      "nombre": "Krystal",
+      "apellido": "Lindsey"
+    },
+    "fechaCreacion": "Sunday, April 19, 2015 4:17 PM"
+  },
+  {
+    "id": 20,
+    "razonSocial": "ISOLOGIX",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=20&w=32&h=32",
+    "direccion": {
+      "calle": "Forrest Street",
+      "altura": 403,
+      "ciudad": "Tilden",
+      "pais": "Myanmar"
+    },
+    "activo": "1620.26",
+    "pasivo": "3916.96",
+    "presidente": {
+      "nombre": "Mcclure",
+      "apellido": "Walter"
+    },
+    "fechaCreacion": "Tuesday, January 20, 2015 3:41 AM"
+  },
+  {
+    "id": 21,
+    "razonSocial": "STELAECOR",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=21&w=32&h=32",
+    "direccion": {
+      "calle": "Middleton Street",
+      "altura": 371,
+      "ciudad": "Oceola",
+      "pais": "Puerto Rico"
+    },
+    "activo": "3028.07",
+    "pasivo": "2442.45",
+    "presidente": {
+      "nombre": "Ann",
+      "apellido": "Rowe"
+    },
+    "fechaCreacion": "Wednesday, May 21, 2014 3:29 AM"
+  },
+  {
+    "id": 22,
+    "razonSocial": "VOLAX",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=22&w=32&h=32",
+    "direccion": {
+      "calle": "Schenectady Avenue",
+      "altura": 226,
+      "ciudad": "Blue",
+      "pais": "Finland"
+    },
+    "activo": "1336.1",
+    "pasivo": "2819.33",
+    "presidente": {
+      "nombre": "Berry",
+      "apellido": "Guy"
+    },
+    "fechaCreacion": "Monday, April 7, 2014 1:25 AM"
+  },
+  {
+    "id": 23,
+    "razonSocial": "ACCUSAGE",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=23&w=32&h=32",
+    "direccion": {
+      "calle": "Stratford Road",
+      "altura": 882,
+      "ciudad": "Lodoga",
+      "pais": "Singapore"
+    },
+    "activo": "2882.81",
+    "pasivo": "1002.13",
+    "presidente": {
+      "nombre": "Romero",
+      "apellido": "Rush"
+    },
+    "fechaCreacion": "Monday, January 26, 2015 5:40 PM"
+  },
+  {
+    "id": 24,
+    "razonSocial": "BRAINCLIP",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=24&w=32&h=32",
+    "direccion": {
+      "calle": "Chauncey Street",
+      "altura": 607,
+      "ciudad": "Lindisfarne",
+      "pais": "Israel"
+    },
+    "activo": "3921.19",
+    "pasivo": "1890.01",
+    "presidente": {
+      "nombre": "Harrison",
+      "apellido": "Booker"
+    },
+    "fechaCreacion": "Thursday, February 20, 2014 7:15 AM"
+  },
+  {
+    "id": 25,
+    "razonSocial": "ENDIPINE",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=25&w=32&h=32",
+    "direccion": {
+      "calle": "Holly Street",
+      "altura": 591,
+      "ciudad": "Linwood",
+      "pais": "Guinea"
+    },
+    "activo": "1086.17",
+    "pasivo": "2650.51",
+    "presidente": {
+      "nombre": "Higgins",
+      "apellido": "Albert"
+    },
+    "fechaCreacion": "Sunday, January 18, 2015 5:36 AM"
+  },
+  {
+    "id": 26,
+    "razonSocial": "MAKINGWAY",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=26&w=32&h=32",
+    "direccion": {
+      "calle": "Alabama Avenue",
+      "altura": 439,
+      "ciudad": "Topaz",
+      "pais": "Norway"
+    },
+    "activo": "1765.95",
+    "pasivo": "3361.73",
+    "presidente": {
+      "nombre": "Dina",
+      "apellido": "Hardy"
+    },
+    "fechaCreacion": "Thursday, May 29, 2014 8:33 AM"
+  },
+  {
+    "id": 27,
+    "razonSocial": "MIXERS",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=27&w=32&h=32",
+    "direccion": {
+      "calle": "Downing Street",
+      "altura": 436,
+      "ciudad": "Charco",
+      "pais": "Cocos (Keeling Islands)"
+    },
+    "activo": "2782.68",
+    "pasivo": "1224.79",
+    "presidente": {
+      "nombre": "Rachel",
+      "apellido": "Wolf"
+    },
+    "fechaCreacion": "Thursday, October 15, 2015 4:24 PM"
+  },
+  {
+    "id": 28,
+    "razonSocial": "VELOS",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=28&w=32&h=32",
+    "direccion": {
+      "calle": "Thames Street",
+      "altura": 998,
+      "ciudad": "Bannock",
+      "pais": "Andorra"
+    },
+    "activo": "3321.63",
+    "pasivo": "1867.9",
+    "presidente": {
+      "nombre": "Susanna",
+      "apellido": "Sanders"
+    },
+    "fechaCreacion": "Wednesday, April 9, 2014 9:46 PM"
+  },
+  {
+    "id": 29,
+    "razonSocial": "KYAGORO",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=29&w=32&h=32",
+    "direccion": {
+      "calle": "Prospect Place",
+      "altura": 709,
+      "ciudad": "Caroleen",
+      "pais": "Cayman Islands"
+    },
+    "activo": "1284.92",
+    "pasivo": "2347.13",
+    "presidente": {
+      "nombre": "Miranda",
+      "apellido": "Sanchez"
+    },
+    "fechaCreacion": "Friday, December 5, 2014 4:23 PM"
+  },
+  {
+    "id": 30,
+    "razonSocial": "NETBOOK",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=30&w=32&h=32",
+    "direccion": {
+      "calle": "Henderson Walk",
+      "altura": 942,
+      "ciudad": "Kidder",
+      "pais": "Namibia"
+    },
+    "activo": "1092.28",
+    "pasivo": "2809.3",
+    "presidente": {
+      "nombre": "Kramer",
+      "apellido": "Carney"
+    },
+    "fechaCreacion": "Thursday, October 9, 2014 10:22 AM"
+  },
+  {
+    "id": 31,
+    "razonSocial": "ZANITY",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=31&w=32&h=32",
+    "direccion": {
+      "calle": "Gunnison Court",
+      "altura": 193,
+      "ciudad": "Inkerman",
+      "pais": "Guyana"
+    },
+    "activo": "2259.67",
+    "pasivo": "2433.75",
+    "presidente": {
+      "nombre": "Dillon",
+      "apellido": "Barrera"
+    },
+    "fechaCreacion": "Sunday, October 5, 2014 12:52 PM"
+  },
+  {
+    "id": 32,
+    "razonSocial": "WEBIOTIC",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=32&w=32&h=32",
+    "direccion": {
+      "calle": "Lafayette Walk",
+      "altura": 696,
+      "ciudad": "Hendersonville",
+      "pais": "Greece"
+    },
+    "activo": "1659.35",
+    "pasivo": "2371.9",
+    "presidente": {
+      "nombre": "Gentry",
+      "apellido": "Lott"
+    },
+    "fechaCreacion": "Monday, October 12, 2015 3:54 PM"
+  },
+  {
+    "id": 33,
+    "razonSocial": "FLEETMIX",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=33&w=32&h=32",
+    "direccion": {
+      "calle": "Railroad Avenue",
+      "altura": 259,
+      "ciudad": "Lutsen",
+      "pais": "Falkland Islands (Malvinas)"
+    },
+    "activo": "2335.75",
+    "pasivo": "3919.56",
+    "presidente": {
+      "nombre": "Betsy",
+      "apellido": "Harper"
+    },
+    "fechaCreacion": "Saturday, March 28, 2015 10:49 PM"
+  },
+  {
+    "id": 34,
+    "razonSocial": "ZENSURE",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=34&w=32&h=32",
+    "direccion": {
+      "calle": "Ashland Place",
+      "altura": 909,
+      "ciudad": "Keller",
+      "pais": "Liechtenstein"
+    },
+    "activo": "2407.93",
+    "pasivo": "2594.53",
+    "presidente": {
+      "nombre": "Cervantes",
+      "apellido": "Ortiz"
+    },
+    "fechaCreacion": "Wednesday, May 14, 2014 6:11 AM"
+  },
+  {
+    "id": 35,
+    "razonSocial": "VALREDA",
+    "logo": "https://placeholdit.imgix.net/~text?txtsize=10&txt=35&w=32&h=32",
+    "direccion": {
+      "calle": "Pulaski Street",
+      "altura": 208,
+      "ciudad": "Matheny",
+      "pais": "Brazil"
+    },
+    "activo": "1611.15",
+    "pasivo": "1787.19",
+    "presidente": {
+      "nombre": "Denise",
+      "apellido": "Roach"
+    },
+    "fechaCreacion": "Monday, April 14, 2014 7:52 PM"
+  }
+];
