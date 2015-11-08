27d5b90160c434790bb4abc0ef7921fe07f1169d
dragonnegro78 <hhhipbgj@sharklasers.com>
2015-11-08 20:49:16 -0300

    divido la tabla de clientes en dos, segun la paridad del campo ID. incorporo chequeo de disponibilidad antes de pedir datos de los clientes.

--- M2/practica/clientes.php
+++ M2/practica/clientes.php
@@ -1,7 +1,34 @@
 <?php
 
-$json = file_get_contents('clientes.json');
+$json = file_get_contents("clientes.json");
 
 $clientes = json_decode($json, true);
 
-echo json_encode($clientes);
+if (! (isset($_GET["filtro"]) && isset($_GET["desde"]) && isset($_GET["cantidad"]))) {
+    exit();
+}
+
+$r = "[";
+
+$i = $_GET["desde"];
+if ($_GET["filtro"] == "impar" && !($i & 1)) {
+    $i++;
+} elseif ($_GET["filtro"] == "par" && $i & 1) {
+    $i++;
+}
+
+for (; $i - $_GET["desde"] < 10 && $i - 1 < count($clientes); $i += 2) {
+    $r .= json_encode($clientes[$i - 1]) . ",";
+}
+
+if (substr($r, -1, 1) == ",") {
+    $r = substr($r, 0, -1);
+}
+
+$r .= "]";
+
+if ($_GET["filtro"] == "impar") {
+    sleep(3);
+}
+
+echo $r;
