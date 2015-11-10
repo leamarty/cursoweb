3272b8a844199f52deca8dda43de062071fef40b
alvarezhm <alvarz.m@gmail.com>
2015-11-10 20:01:20 -0300

    en un rato sigo

--- M2/practica/clientes.php
+++ M2/practica/clientes.php
@@ -3,12 +3,25 @@
 $json = file_get_contents('clientes.json');
 
 $clientes = json_decode($json, true);
-
-if($_REQUEST['id']==0){
-
-}
+$i=1;
 
 
+//if ($_REQUEST['cond'] == 'p') {
 
+    //for ($i = 0; $i < 5; $i++) {
+        //if ($i != 0 && ($i % 2) == 1) {
+            $pares = array(
+                'logo' => $clientes[$i]['logo'],
+                'id' => $clientes[$i]['id'],
+                'razs' => $clientes[$i]['razonSocial'],
+                'pres' => $clientes[$i]['presidente'],
+                'patri' => "1",
+            );
+        //}
+    //}
 
-echo json_encode($clientes[$indice-1]);
+//}
+echo json_encode($pares);
+
+//}
+
