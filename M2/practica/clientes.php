bee773b650e43cc091c67a2cc9633e5229ce3cfd
alvarezhm <alvarz.m@gmail.com>
2015-11-10 00:53:45 -0300

    en un rato sigo

--- M2/practica/clientes.php
+++ M2/practica/clientes.php
@@ -4,4 +4,11 @@ $json = file_get_contents('clientes.json');
 
 $clientes = json_decode($json, true);
 
-echo json_encode($clientes);
+if($_REQUEST['id']==0){
+
+}
+
+
+
+
+echo json_encode($clientes[$indice-1]);
