352df4d69abba3cb4f40a469ef7e2bca37234e77
dragonnegro78 <hhhipbgj@sharklasers.com>
2015-11-11 20:20:10 -0300

    Revirtiendo commits de pepe a la branch incorrecta. Impiden mergear las otras branches.
    This reverts commit bee773b650e43cc091c67a2cc9633e5229ce3cfd.

--- M2/practica/clientes.php
+++ M2/practica/clientes.php
@@ -4,11 +4,4 @@ $json = file_get_contents('clientes.json');
 
 $clientes = json_decode($json, true);
 
-if($_REQUEST['id']==0){
-
-}
-
-
-
-
-echo json_encode($clientes[$indice-1]);
+echo json_encode($clientes);
