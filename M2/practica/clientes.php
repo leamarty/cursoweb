472925b8f0df5d400fb1fb608d63bf872338bbac
dragonnegro78 <hhhipbgj@sharklasers.com>
2015-11-11 01:57:46 -0300

    clientes.php: elimino la variable resultado que se mandaba por ajax cuando se pedian clientes y simplemente respondo como fail (401) si el resultado era error. index.html: muevo el getjson a una funcion propia para no tener que hacer hacks por la mutabilidad de una variable. agrego ademas caso en el que getjson devuelva error (cuando clientes.php detecta un error en los inputs)

--- M2/practica/clientes.php
+++ M2/practica/clientes.php
@@ -6,22 +6,18 @@ $clientes = json_decode($json, true);
 
 // falta comprobar que sean numeros
 if (!(isset($_GET["filtro"]) && isset($_GET["desde"]) && isset($_GET["cantidad"])) ||
-    ($_GET["filtro"] != "impar" && $_GET["filtro"] != "par") ||
+    !in_array($_GET["filtro"], array("impar", "par")) ||
     $_GET["desde"] < 1 /* || $_GET["desde"] > count($clientes) */
 ) {
-    $res = array(
-        "resultado" => "error"
-    );
-    exit(json_encode($res));
+    header("HTTP/1.1 401 Unauthorized");
+    exit();
 }
 
-$res = array(
-    "resultado" => "success"
-);
+$res = array();
 
 for ($i = $_GET["desde"]; $i - $_GET["desde"] < $_GET["cantidad"] && $i - 1 < count($clientes); $i++) {
     if ($i % 2 == ($_GET["filtro"] == "impar" ? 1 : 0)) {
-        $res["clientes"][] = $clientes[$i - 1];
+        $res[] = $clientes[$i - 1];
     }
 }
 
