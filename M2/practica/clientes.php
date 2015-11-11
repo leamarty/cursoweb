fdec99c90d7dd8fa9fb33109e872b907be19a6e0
alvarezhm <alvarz.m@gmail.com>
2015-11-11 19:03:54 -0300

    falta un poco :$

--- M2/practica/clientes.php
+++ M2/practica/clientes.php
@@ -3,25 +3,43 @@
 $json = file_get_contents('clientes.json');
 
 $clientes = json_decode($json, true);
-$i=1;
 
 
 //if ($_REQUEST['cond'] == 'p') {
 
-    //for ($i = 0; $i < 5; $i++) {
-        //if ($i != 0 && ($i % 2) == 1) {
-            $pares = array(
-                'logo' => $clientes[$i]['logo'],
-                'id' => $clientes[$i]['id'],
-                'razs' => $clientes[$i]['razonSocial'],
-                'pres' => $clientes[$i]['presidente'],
-                'patri' => "1",
-            );
-        //}
-    //}
+$a=$_REQUEST['cond'];
+$i=$_REQUEST['in'];
+$c=$_REQUEST['can'];
+while($i<$c){
+    if ( ($i % 2) ==1 && $a=='p' ) {
+        $rta[$i] = array(
+            'logo' => $clientes[$i]['logo'],
+            'id' => $clientes[$i]['id'],
+            'razs' => $clientes[$i]['razonSocial'],
+            'pres' => $clientes[$i]['presidente'],
+            'patri' => "1"
+        );
+        }else{if( ($i%2)==0 && $a=='i') {
+        $rta[$i] = array(
+            'logo' => $clientes[$i]['logo'],
+            'id' => $clientes[$i]['id'],
+            'razs' => $clientes[$i]['razonSocial'],
+            'pres' => $clientes[$i]['presidente'],
+            'patri' => "1"
+        );
+    }
+    }
 
-//}
-echo json_encode($pares);
+$i++;
 
-//}
+}
+if($a=='i'){
+    sleep(3);
+}
+echo json_encode($rta);
+
+
+
+
+
 
