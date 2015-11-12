f2999dd796d18c92848b5c91bbb74eff17a7377b
dragonnegro78 <hhhipbgj@sharklasers.com>
2015-11-12 13:25:22 -0300

    unifico funciones de controllers y evito errores de mutabilidad

--- M2/practica/controllers.js
+++ M2/practica/controllers.js
@@ -1,58 +1,37 @@
 var clientesProviderApp = angular.module('clientesProviderApp', []);
 
+function pedir_clientes($scope, $http, paridad) {
+    $http.get(
+        'clientes.php', {
+            cache: false,
+            params: {
+                desde: $("tbody#tbody-" + paridad).children("tr").length * 2 + 1,
+                cantidad: 10,
+                filtro: paridad
+            }
+        }).success(function (data) {
+            $(data).each(function () {
+                $scope.clientes.push(this);
+            });
+        });
+}
+
 clientesProviderApp.controller('ClientesListCtrl', ['$scope', '$http',
 
     function ($scope, $http) {
 
         $scope.pedirClientes = function () {
-            $http.get(
-                'clientes.php', {
-                    cache: false,
-                    params: {
-                        desde: $scope.desde,
-                        cantidad: $scope.cantidad,
-                        filtro: "impar"
-                    }
-                }).success(function (data) {
-                    $(data).each(function () {
-                        $scope.clientes.push(this);
-                    });
-                    // scope.desde = scope.desde + scope.cantidad;
-                });
-            $http.get(
-                'clientes.php', {
-                    cache: false,
-                    params: {
-                        desde: $scope.desde,
-                        cantidad: $scope.cantidad,
-                        filtro: "par"
-                    }
-                }).success(function (data) {
-                    $(data).each(function () {
-                        $scope.clientes.push(this);
-                    });
-                    // scope.desde = scope.desde + scope.cantidad;
-                });
+            for (var paridad in {impar: "", par: ""}) {
+                pedir_clientes($scope, $http, paridad);
+            }
         };
     }]);
 
-clientesProviderApp.filter('impar', function() {
-    return function (input) {
+clientesProviderApp.filter("filtrarParidad", function () {
+    return function (input, paridad) {
         var r = [];
         for (var i in input) {
-            if (input[i].id % 2 == 1) {
-                r.push(input[i]);
-            }
-        }
-        return r;
-    }
-});
-
-clientesProviderApp.filter('par', function() {
-    return function (input) {
-        var r = [];
-        for (var i in input) {
-            if (input[i].id % 2 == 0) {
+            if (input[i].id % 2 == (paridad == "impar" ? 1 : 0)) {
                 r.push(input[i]);
             }
         }
