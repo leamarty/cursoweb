9a69031f0d119bff78b827baf4b3ba2a1e1edb63
alvarezhm <alvarz.m@gmail.com>
2015-11-13 11:55:45 -0300

    pepe paractica m2 - semi terminada

--- M2/practica/clientes.php
+++ M2/practica/clientes.php
@@ -4,4 +4,42 @@ $json = file_get_contents('clientes.json');
 
 $clientes = json_decode($json, true);
 
-echo json_encode($clientes);
+
+//if ($_REQUEST['cond'] == 'p') {
+
+$a=$_REQUEST['cond'];
+$i=$_REQUEST['in'];
+$c=$_REQUEST['can'];
+while($i<$c){
+    if ( ($i % 2) ==1 && $a=='p' ) {
+        $rta[$i] = array(
+            'logo' => $clientes[$i]['logo'],
+            'id' => $clientes[$i]['id'],
+            'razs' => $clientes[$i]['razonSocial'],
+            'pres' => $clientes[$i]['presidente'],
+            'patri' => "1"
+        );
+    }else{if( ($i%2)==0 && $a=='i') {
+        $rta[$i] = array(
+            'logo' => $clientes[$i]['logo'],
+            'id' => $clientes[$i]['id'],
+            'razs' => $clientes[$i]['razonSocial'],
+            'pres' => $clientes[$i]['presidente'],
+            'patri' => "1"
+        );
+    }
+    }
+
+    $i++;
+
+}
+if($a=='i'){
+    sleep(3);
+}
+echo json_encode($rta);
+
+
+
+
+
+
