690b8b7167642c739f622dd4c0f08bf47fdb87b3
Leandro <leanmarty@gmail.com>
2015-11-21 18:33:32 -0300

    Subo la práctica 3

---
+++ M3/practica/clientes.php
@@ -0,0 +1,158 @@
+<?php
+
+abstract class AutoLlenable {
+    protected $mapper = array();
+
+    public function __construct($datos = array()) {
+        foreach ($datos as $key => $value) {
+            if (isset($this->mapper[$key])) {
+                $key = $this->mapper[$key];
+            }
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
+}
+
+class Cliente extends AutoLlenable {
+    public $id;
+    public $razonSocial;
+    public $logo;
+    public $direccion;
+    public $activo;
+    public $pasivo;
+    public $presidente;
+    public $fechaCreacion;
+
+    protected $mapper = array(
+        'razon_social' => 'razonSocial',
+        'fecha_creacion' => 'fechaCreacion'
+    );
+
+    public function __construct($datos) {
+        parent::__construct($datos);
+
+        $this->direccion = new Direccion(
+            array(
+                'direccion_calle' => $datos['direccion_calle'],
+                'direccion_altura' => $datos['direccion_altura'],
+                'direccion_ciudad' => $datos['direccion_ciudad'],
+                'direccion_pais' =>  $datos['direccion_pais']
+            )
+        );
+        $this->presidente = new Persona(
+            array(
+                'presidente_nombre' => $datos['presidente_nombre'],
+                'presidente_apellido' =>  $datos['presidente_apellido']
+            )
+        );
+    }
+
+    public static function buscar(mysqli $db, $cantidad, $desde, $filtro) {
+        $q = 'SELECT * FROM clientes WHERE id >= ' . $desde . ' AND id % 2 = ' . ($filtro == 'par' ? 0 : 1) . ' LIMIT ' . $cantidad;
+        $listaDeClientes = $db->query($q)->fetch_all(MYSQLI_ASSOC);
+
+        $arrayFinal = array();
+        foreach ($listaDeClientes as $item) {
+            $cliente = new Cliente($item);
+            $arrayFinal[] = $cliente;
+        }
+        return $arrayFinal;
+    }
+
+    /**
+     * @param mysqli $db
+     * @param        $id
+     *
+     * @return null|Cliente
+     */
+    public static function buscarPorId(mysqli $db, $id) {
+        $cliente = Cliente::buscar($db, 1, $id, $id % 2 ? 'impar' : 'par');
+        return count($cliente) ? $cliente[0] : null;
+    }
+
+    public function borrar(mysqli $db) {
+        $q = 'DELETE FROM clientes WHERE id = ' . $this->id . ';';
+        $db->query($q);
+        // TODO chequear errores
+    }
+}
+
+class Direccion extends AutoLlenable {
+    public $calle;
+    public $altura;
+    public $ciudad;
+    public $pais;
+
+    protected $mapper = array(
+        'direccion_calle' => 'calle',
+        'direccion_altura' => 'altura',
+        'direccion_ciudad' => 'ciudad',
+        'direccion_pais' =>  'pais'
+    );
+}
+
+class Persona extends AutoLlenable {
+    public $nombre;
+    public $apellido;
+
+    protected $mapper = array(
+        'presidente_nombre' => 'nombre',
+        'presidente_apellido' =>  'apellido'
+    );
+}
+
+class ClienteController {
+    public function get() {
+        $desde = (int) $_GET['desde'];
+        $cantidad = (int) $_GET['cantidad'];
+        $filtro = $_GET['filtro'];
+
+        if (!$desde || !$cantidad || !in_array($filtro, array('par', 'impar'))) {
+            header('HTTP/1.1 400 Bad Request');
+            echo json_encode(
+                array('code' => 400, 'message' => 'No se recibieron correctamente los parámetros "desde", "cantidad" y "filtro"')
+            );
+            exit();
+        }
+
+        echo json_encode(Cliente::buscar($this->getDb(), $cantidad, $desde, $filtro));
+    }
+    public function post() {
+
+    }
+    public function delete() {
+        parse_str(file_get_contents('php://input'), $_DELETE);
+
+        $cliente = Cliente::buscarPorId($this->getDb(), $_DELETE['id']);
+        if ($cliente) {
+            $cliente->borrar($this->getDb());
+        } else {
+            header('HTTP/1.1 404 Not Found');
+            echo json_encode(
+                array('code' => 404, 'message' => 'No se encontraron clientes con ese id')
+            );
+        }
+    }
+    private function getDb() {
+        return new mysqli('localhost', 'root', '', 'cursoweb');
+    }
+}
+
+// El request ARRANCA acá
+
+header('Content-Type: application/json');
+
+$handler = new ClienteController();
+
+$metodo = strtolower($_SERVER['REQUEST_METHOD']);
+if (method_exists($handler, $metodo)) {
+    $handler->$metodo();
+} else {
+    header('HTTP/1.1 400 Bad Request');
+
+    echo json_encode(
+        array('code' => 400, 'message' => 'No existe el método "' . $metodo . '"')
+    );
+}
